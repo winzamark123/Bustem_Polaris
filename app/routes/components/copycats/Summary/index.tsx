2427cf56a2b55ebdffad77e59efe77a950932c7d
Win Cheng <winzamark12@gmail.com>
2025-01-26 10:07:17 -0800

    Summary complete

--- app/routes/components/copycats/Summary/index.tsx
+++ app/routes/components/copycats/Summary/index.tsx
@@ -1,26 +1,47 @@
-import InfoCard from '@ui/InfoCard';
 import CopycatSummaryHeader from './header';
 import CopycatSummaryItem from './item';
+import { BlockStack, Box, Card, InlineGrid } from '@shopify/polaris';
+import { AlertTriangleIcon, HashtagIcon, PageIcon  } from '@shopify/polaris-icons';
 
 const summaryData = [
   {
     title: 'Total Number of Infringements',
     value: '0',
+    icon: PageIcon,
   },
   {
     title: 'Combined Traffic',
     value: 'Coming Soon',
+    icon: HashtagIcon,
   },
   {
     title: 'Estimated Loss',
     value: 'Coming Soon',
+    icon: AlertTriangleIcon,
+    warning: true,
   },
 ];
 
 export default function CopycatSummary() {
   return (
     <main>
-      <InfoCard header={<CopycatSummaryHeader />} items={summaryData} ItemLayout={CopycatSummaryItem} />
+    <Card>
+      <BlockStack gap="400">
+        <CopycatSummaryHeader />
+        {summaryData && summaryData.length > 0 && (
+          <InlineGrid columns={summaryData.length} gap="400">
+            {summaryData.map((item, index) => (
+              <Box key={index}>
+                <CopycatSummaryItem 
+                  {...item} 
+                  tone={item.warning ? "critical" : undefined}
+                        />
+              </Box>
+            ))}
+          </InlineGrid>
+        )}
+      </BlockStack>
+    </Card>
     </main>
   );
 }
--- app/routes/components/copycats/Summary/item.tsx
+++ app/routes/components/copycats/Summary/item.tsx
@@ -5,26 +5,36 @@ import {
     Card,
     InlineStack,
     Icon,
+    IconSource,
+    Badge,
   } from '@shopify/polaris';
-  import { PersonFilledIcon } from '@shopify/polaris-icons';
 
   interface CopycatSummaryItemProps {
     title: string;
     value: string;
-    subtitle: string;
+    icon: React.ElementType;
+    tone?: "critical";
   }
   
-  export default function CopycatSummaryItem({ title, value, subtitle }: CopycatSummaryItemProps) {
+  export default function CopycatSummaryItem({ 
+    title, 
+    value, 
+    icon,
+    tone,
+  }: CopycatSummaryItemProps) {
     return (
         <Card>
             <BlockStack gap="200">
-          <InlineStack gap="300" align="start" blockAlign="center">
-            <Box background="bg-fill-secondary" borderRadius="200" padding="100">
-              <Icon source={PersonFilledIcon} />
-            </Box>
-            <Text as="h3" variant="headingLg">{title}</Text>
-          </InlineStack>
-                <Text as="h3" variant="headingXl">{value}</Text>
+              <InlineStack gap="300" align="start" blockAlign="center">
+                <Box background="bg-fill-secondary" borderRadius="200" padding="100">
+                  <Icon source={icon as unknown as IconSource} tone={tone} />
+                </Box>
+                <Text as="h3" variant="headingMd">{title}</Text>
+              </InlineStack>
+              <InlineStack gap="300" align="start" blockAlign="center">
+                <Text as="h3" variant="headingLg">{value}</Text>
+                {tone && <Badge tone={tone}>03/21/25</Badge>}
+              </InlineStack>
             </BlockStack>
       </Card>
     );
